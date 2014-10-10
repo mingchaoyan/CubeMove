5b44940e582dd28176f050640b49d9f4c9cc64c6
mingchaoyan <mingchaoyan@gmail.com>
2014-10-10 23:14:53 +0800

    添加移动代码，调整速度

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float amtToMove = Input.GetAxis("Horizontal") * speed;
+        gameObject.transform.Translate(Vector3.right * amtToMove);
+	
+	}
+}
